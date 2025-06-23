a6aae1a1979c53c71c3e6fcfdba9916ec2d63bf0
yuanhes <5900664+yuanhes@users.noreply.github.com>
2025-06-23 17:53:40 +0200

    Lab09 (#2) cs61a: complete lab09
    * cs61a: complete lab09
    
    * remove trash files

--- cs61a/lab09/lab09.scm
+++ cs61a/lab09/lab09.scm
@@ -1,10 +1,21 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (if (< num1 num2) -1 
+      (if (= num1 num2) 0 1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (x) (+ x num))  
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+    (lambda (x)
+      (if (= n 1) (f x)
+        (f ((repeat f (- n 1)) x))))
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +27,10 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (define ma (max a b))
+  (define mi (min a b))
+  (define mo (modulo ma mi))
+  (if (zero? mo) mi
+    (gcd mi mo))
+)
